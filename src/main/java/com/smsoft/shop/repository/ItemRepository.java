21117026a4e0c0f9ddc410f48ee676f3e36525b3
kimsumin <tnals1564@naver.com>
2023-10-18 17:12:18 +0900

    querydsl predicate 테스트

--- src/main/java/com/smsoft/shop/repository/ItemRepository.java
+++ src/main/java/com/smsoft/shop/repository/ItemRepository.java
@@ -3,11 +3,12 @@ package com.smsoft.shop.repository;
 import com.smsoft.shop.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ItemRepository extends JpaRepository<Item, Long> {
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
     List<Item> findByItemNm(String itemNm);
     List<Item> findByPriceLessThan(Integer price);
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
--- src/test/java/com/smsoft/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/smsoft/shop/repository/ItemRepositoryTest.java
@@ -1,12 +1,23 @@
 package com.smsoft.shop.repository;
 
+import com.querydsl.core.BooleanBuilder;
+import com.querydsl.core.types.Predicate;
+import com.querydsl.jpa.impl.JPAQuery;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.smsoft.shop.constant.ItemSellStatus;
 import com.smsoft.shop.entity.Item;
+import com.smsoft.shop.entity.QItem;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.context.TestPropertySource;
+import org.thymeleaf.util.StringUtils;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -17,6 +28,8 @@ import static org.junit.jupiter.api.Assertions.*;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ItemRepositoryTest {
 
+    @PersistenceContext
+    EntityManager em;
     @Autowired
     ItemRepository itemRepository;
 
@@ -105,6 +118,84 @@ class ItemRepositoryTest {
         List<Item> itemList = itemRepository.findByItemDetailByNative("상품 상세설명");
 
         for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @Order(8)
+    @DisplayName("Querydsl 조회 테스트1")
+    public void queryDslTest() {
+        JPAQueryFactory queryFactory = new JPAQueryFactory(em);
+
+        QItem qItem = QItem.item;
+        JPAQuery<Item> query = queryFactory.selectFrom(qItem)
+                .where(qItem.itemSellStatus.eq(ItemSellStatus.SELL))
+                .where(qItem.itemDetail.like("%상품 상세설명%"))
+                .orderBy(qItem.price.desc());
+
+        List<Item> itemList = query.fetch();
+
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
+    }
+
+    public void createItemList2() {
+        for(int i=1; i<=5; i++) {
+            Item item = new Item();
+            item.setItemNm("상품" + i);
+            item.setPrice(10000 + i);
+            item.setStockNumber(100);
+            item.setItemDetail("상품 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+
+            itemRepository.save(item);
+        }
+
+        for(int i=6; i<=10; i++) {
+            Item item = new Item();
+            item.setItemNm("상품" + i);
+            item.setPrice(10000 + i);
+            item.setStockNumber(100);
+            item.setItemDetail("상품 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SOLD_OUT);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+
+            itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @Order(9)
+    @DisplayName("Querydsl 조회 테스트2")
+    public void queryDslTest2() {
+        this.createItemList2();
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        QItem qItem = QItem.item;
+
+        String itemDetail = "상품 설명";
+        int price = 10003;
+        String itemSellStat = "SELL";
+
+        booleanBuilder.and(qItem.itemDetail.like("%" + itemDetail + "%"));
+        booleanBuilder.and(qItem.price.gt(price));
+        System.out.println(ItemSellStatus.SELL);
+
+        if(StringUtils.equals(itemSellStat, ItemSellStatus.SELL)) {
+            booleanBuilder.and(qItem.itemSellStatus.eq(ItemSellStatus.SELL));
+        }
+
+        Pageable pageable = PageRequest.of(0, 5);
+        Page<Item> itemPagingResult = itemRepository.findAll(booleanBuilder, pageable);
+        System.out.println("total elements : " + itemPagingResult.getTotalElements());
+
+        List<Item> resultItemList = itemPagingResult.getContent();
+        for (Item item: resultItemList) {
             System.out.println(item.toString());
         }
     }
