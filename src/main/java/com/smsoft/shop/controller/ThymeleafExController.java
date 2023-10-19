cfe6f2608e7a7dc99401b474a269fca3ecfeea69
kimsumin <tnals1564@naver.com>
2023-10-19 11:54:01 +0900

    thymeleaf th:each 사용

--- src/main/java/com/smsoft/shop/controller/ThymeleafExController.java
+++ src/main/java/com/smsoft/shop/controller/ThymeleafExController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(value = "/thymeleaf")
@@ -28,5 +30,24 @@ public class ThymeleafExController {
         model.addAttribute("itemDto", itemDto);
 
         return "thymeleafex/thymeleafEx02";
+    }
+
+    @GetMapping(value = "/ex03")
+    public String thymeleafExample03(Model model) {
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (int i=0; i<5; i++) {
+            ItemDto itemDto = new ItemDto();
+            itemDto.setItemNm("상품" + i);
+            itemDto.setPrice(3000 + i);
+            itemDto.setItemDetail("상품 상세" + i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+
+        model.addAttribute("itemDtoList", itemDtoList);
+
+        return "thymeleafex/thymeleafEx03";
     }
 }
