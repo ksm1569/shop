dc0059b2933b4839a0f6117a2545f0227d910cb0
ksm1569 <tnals1564@naver.com>
2023-10-21 18:19:17 +0900

    회원가입 테스트 완료, spring validation 추가

--- src/main/java/com/smsoft/shop/dto/MemberFormDto.java
+++ src/main/java/com/smsoft/shop/dto/MemberFormDto.java
@@ -3,11 +3,22 @@ package com.smsoft.shop.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 @Getter
 @Setter
 public class MemberFormDto {
+    @NotBlank(message = "이름을 입력해주세요")
     private String name;
+
+    @NotBlank(message = "이메일을 입력해주세요")
+    @Email(message = "이메일 형식으로 입력해주세요")
     private String email;
+
+    @NotBlank(message = "비밀번호를 입력해주세요")
     private String password;
+
+    @NotBlank(message = "주소를 입력해주세요")
     private String address;
 }
