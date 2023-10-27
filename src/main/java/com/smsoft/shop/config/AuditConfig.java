2e5a268eb2126d96d017740ae041a58851609435
ksm1569 <tnals1564@naver.com>
2023-10-27 20:41:17 +0900

    JPA Auditing 활성화

---
+++ src/main/java/com/smsoft/shop/config/AuditConfig.java
@@ -0,0 +1,16 @@
+package com.smsoft.shop.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class AuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        return new AuditorAwareImpl();
+    }
+}
