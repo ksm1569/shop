e0dc3c7207da5eca069cda7124c1d3b631d40b37
kimsumin <tnals1564@naver.com>
2023-11-10 18:00:14 +0900

    테스트 마무리 못함

--- src/test/java/com/smsoft/shop/service/OrderServiceTest.java
+++ src/test/java/com/smsoft/shop/service/OrderServiceTest.java
@@ -1,7 +1,81 @@
 package com.smsoft.shop.service;
 
+import com.smsoft.shop.constant.ItemSellStatus;
+import com.smsoft.shop.dto.OrderDto;
+import com.smsoft.shop.entity.Item;
+import com.smsoft.shop.entity.Member;
+import com.smsoft.shop.entity.Order;
+import com.smsoft.shop.entity.OrderItem;
+import com.smsoft.shop.repository.ItemRepository;
+import com.smsoft.shop.repository.MemberRepository;
+import com.smsoft.shop.repository.OrderRepository;
+import org.aspectj.weaver.ast.Or;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestPropertySource(locations = "classpath:application-test.properties")
+@Transactional
+@SpringBootTest
 class OrderServiceTest {
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    public Item saveItem() {
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(3000);
+        item.setStockNumber(100);
+        item.setStockNumber(100);
+        item.setItemDetail("상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+
+        return itemRepository.save(item);
+    }
+
+    public Member saveMember() {
+        Member member = new Member();
+        member.setEmail("tnals1569@gmail.com");
+
+        return memberRepository.save(member);
+    }
+
+    @Test
+    @DisplayName("주문 테스트")
+    public void order() {
+        Item item = saveItem();
+        Member member = saveMember();
+
+        OrderDto orderDto = new OrderDto();
+        orderDto.setCount(10);
+        orderDto.setItemId(item.getId());
+
+        Long orderId = orderService.order(orderDto, member.getEmail());
+        Order order = orderRepository.findById(orderId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        List<OrderItem> orderItems = order.getOrderItems();
+
+        //int totalPrice =
+    }
+
 
 }
