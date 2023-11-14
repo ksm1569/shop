f0c4e032031a969a7dff92cc013f7b64e2350195
kimsumin <tnals1564@naver.com>
2023-11-14 17:53:52 +0900

    장바구니 담기 백엔드 로직 추가

---
+++ src/main/java/com/smsoft/shop/service/CartService.java
@@ -0,0 +1,55 @@
+package com.smsoft.shop.service;
+
+import com.smsoft.shop.dto.CartItemDto;
+import com.smsoft.shop.entity.Cart;
+import com.smsoft.shop.entity.CartItem;
+import com.smsoft.shop.entity.Item;
+import com.smsoft.shop.entity.Member;
+import com.smsoft.shop.repository.CartItemRepository;
+import com.smsoft.shop.repository.CartRepository;
+import com.smsoft.shop.repository.ItemRepository;
+import com.smsoft.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
+
+
+@RequiredArgsConstructor
+@Transactional
+@Service
+public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+
+    public Long addCart(CartItemDto cartItemDto, String email) {
+        Item item = itemRepository.findById(cartItemDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+
+        Member member = memberRepository.findByEmail(email);
+
+        Cart cart = cartRepository.findByMemberId(member.getId());
+
+        if(cart==null) {
+            cart = Cart.createCart(member);
+            cartRepository.save(cart);
+        }
+
+        CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
+
+        if (savedCartItem==null) {
+            savedCartItem.addCount(cartItemDto.getCount());
+
+            return savedCartItem.getId();
+        } else {
+            CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
+            cartItemRepository.save(cartItem);
+
+            return cartItem.getId();
+        }
+    }
+}
