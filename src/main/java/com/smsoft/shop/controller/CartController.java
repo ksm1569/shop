0d158f996e73e2406db7b068895d1c346d65a551
kimsumin <tnals1564@naver.com>
2023-11-15 12:02:07 +0900

    장바구니 수량 변경기능 완료

--- src/main/java/com/smsoft/shop/controller/CartController.java
+++ src/main/java/com/smsoft/shop/controller/CartController.java
@@ -1,16 +1,16 @@
 package com.smsoft.shop.controller;
 
+import com.smsoft.shop.dto.CartDetailDto;
 import com.smsoft.shop.dto.CartItemDto;
 import com.smsoft.shop.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
@@ -47,6 +47,28 @@ public class CartController {
         } catch (Exception e) {
             return new ResponseEntity<String>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
+
+        return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/cart")
+    public String orderHist(Principal principal, Model model) {
+        List<CartDetailDto> cartDetailDtoList = cartService.getCartList(principal.getName());
+        model.addAttribute("cartItems", cartDetailDtoList);
+
+        return "cart/cartList";
+    }
+
+    @PatchMapping(value = "/cartItem/{cartItemId}")
+    @ResponseBody
+    public ResponseEntity updateCartItem(@PathVariable("cartItemId") Long cartItemId, int count, Principal principal) {
+        if (count <= 0) {
+            return new ResponseEntity<String>("최소 1개 이상 담아야 합니다.", HttpStatus.BAD_REQUEST);
+        } else if (!cartService.validateCartItem(cartItemId, principal.getName())) {
+            return new ResponseEntity<String>("수정 권한이 없습니다", HttpStatus.FORBIDDEN);
+        }
+
+        cartService.updateCartItemCount(cartItemId, count);
 
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
     }
--- src/main/java/com/smsoft/shop/service/CartService.java
+++ src/main/java/com/smsoft/shop/service/CartService.java
@@ -1,5 +1,6 @@
 package com.smsoft.shop.service;
 
+import com.smsoft.shop.dto.CartDetailDto;
 import com.smsoft.shop.dto.CartItemDto;
 import com.smsoft.shop.entity.Cart;
 import com.smsoft.shop.entity.CartItem;
@@ -12,8 +13,11 @@ import com.smsoft.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.thymeleaf.util.StringUtils;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RequiredArgsConstructor
@@ -51,5 +55,42 @@ public class CartService {
 
             return cartItem.getId();
         }
+    }
+
+    @Transactional(readOnly = true)
+    public List<CartDetailDto> getCartList(String email) {
+        List<CartDetailDto> cartDetailDtoList = new ArrayList<>();
+
+        Member member = memberRepository.findByEmail(email);
+        Cart cart = cartRepository.findByMemberId(member.getId());
+
+        if (cart == null) {
+            return cartDetailDtoList;
+        }
+
+        cartDetailDtoList = cartItemRepository.findCartDetailDtoList(cart.getId());
+
+        return cartDetailDtoList;
+    }
+
+    @Transactional(readOnly = true)
+    public boolean validateCartItem(Long cartItemId, String email) {
+        Member curMember = memberRepository.findByEmail(email);
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+        Member savedMember = cartItem.getCart().getMember();
+
+        if(!StringUtils.equals(curMember.getEmail(), savedMember.getEmail())) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void updateCartItemCount(Long cartItemId, int count) {
+        CartItem cartItem = cartItemRepository.findById(cartItemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        cartItem.updateCount(count);
     }
 }
